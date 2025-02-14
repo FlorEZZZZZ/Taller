09c853f20af9a93e90beaab975f7e75b27c99d2b
APRENDIZ <APRENDIZ@STMAPRRMDFSP641.FormacionSena.RED>
2025-02-14 15:20:54 -0500

    Actualizado

---
+++ src/Controlador/Principal2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+/**
+ *
+ * @author APRENDIZ
+ */
+public class Principal2 {
+
+    public static void main(String[] args) {
+    }
+}
