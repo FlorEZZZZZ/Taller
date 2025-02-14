7c4592c07b9df3a1bef12b97bf7932615b75279b
APRENDIZ <APRENDIZ@STMAPRRMDFSP641.FormacionSena.RED>
2025-02-14 14:51:12 -0500

    Actualizado

---
+++ src/Controlador/Principal.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author APRENDIZ
+ */
+public class Principal {
+    
+        public static void main(String[] args) {
+        
+            Scanner sc = new Scanner(System.in);
+        
+            System.out.print("Digite el tamaño del vector: ");
+            int tamañoVector = sc.nextInt();
+        
+        }
+}
